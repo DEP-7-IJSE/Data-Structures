4fb220025217ce13a55aec09639392e913e99a63
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-05-06 12:46:25 +0530

    Queue Module Structure

---
+++ Queue/src/Queue.java
@@ -0,0 +1,28 @@
+public class Queue {
+    private int[] dataArray;
+
+    public void enQueue(int data){
+
+    }
+    public void deQueue(){
+
+    }
+    public void clear(){
+
+    }
+    public void printQueue(){
+
+    }
+    public boolean contains(int data){
+        return false;
+    }
+    public int peek(){
+        return 0;
+    }
+    public boolean isEmpty(){
+        return false;
+    }
+    public int size(){
+        return 0;
+    }
+}
