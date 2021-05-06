dfb6de9ccb96493c41f40c980029aa29a4c8209e
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-05-06 12:50:50 +0530

    enQueue & printQueue Methods

--- Queue/src/Queue.java
+++ Queue/src/Queue.java
@@ -2,16 +2,30 @@ public class Queue {
     private int[] dataArray;
 
     public void enQueue(int data){
-
+        if(isEmpty()){
+            dataArray=new int[1];
+            dataArray[0]=data;
+        }else{
+            int [] temp=new int[dataArray.length+1];
+            for (int i = 0; i < dataArray.length; i++) {
+                temp[i]=dataArray[i];
+            }
+            temp[temp.length -1]=data;
+            dataArray=temp;
+        }
     }
     public void deQueue(){
 
     }
     public void clear(){
-
+        dataArray = null;
     }
     public void printQueue(){
-
+        System.out.print("[");
+        for(int i=dataArray.length-1; i>=0; i--){
+            System.out.print(dataArray[i]+", ");
+        }
+        System.out.println(isEmpty() ? "empty]":"\b\b]");
     }
     public boolean contains(int data){
         return false;
@@ -20,7 +34,7 @@ public class Queue {
         return 0;
     }
     public boolean isEmpty(){
-        return false;
+        return dataArray==null;
     }
     public int size(){
         return 0;
