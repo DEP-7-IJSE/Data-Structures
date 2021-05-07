bb9e8696e4c5e247cc4a75ff5dd049eb01f66dc9
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-05-07 11:43:57 +0530

    DynamicArray class structure

---
+++ DynamicArray/src/ds/DynamicArray.java
@@ -0,0 +1,32 @@
+package ds;
+
+public class DynamicArray {
+
+    public void add(int data){
+
+    }
+    public void add(int index,int data){
+
+    }
+    public void remove(int index){
+
+    }
+    public void get(int index){
+
+    }
+    public void print(){
+
+    }
+    public void clear(){
+
+    }
+    public int size(){
+        return 0;
+    }
+    public boolean contains(int data){
+        return false;
+    }
+    public boolean isEmpty(){
+        return false;
+    }
+}
